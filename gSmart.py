5b909ac76b6b3518ab2ff1d13b7108ea29018ea8
Garrett <gdgrube@gmail.com>
2018-12-19 13:14:55 -0500

    Add files via upload

--- gSmart.py
+++ gSmart.py
@@ -22,29 +22,9 @@ class gSmart:
 		self.chkW = 50
 		self.chkmtW = 1000
 
-	def getNextMove(self, b):
-	
-		#gt = buildGameTree()
-		#move = miniMax(gt)
-		
-		#replace below with minimax algo with alpha beta pruning
-		
-		#pcs = b.getPieces(b.turn)
-		#bestMove = []
-		#bestPosition = -200;
-		#moves = self.getAllNextMoves(b)
-		#for m in moves:
-		#	pos = self.evaluatePosition(m[0])
-		#	if b.turn == 'b':
-		#		ps = pos[0]
-		#	else:
-		#		ps = pos[1]
-		#	if ps > bestPosition:
-		#		bestPosition = ps
-		#		bestMove = m
-		#print("The best move is: " + str(bestMove))
-		
-		return bestMove[1]
+	def getNextMove(self, b, n):
+		gt = gameTree(b, n)		#create a gameTree of n ply
+		return gt.miniMax()		#use miniMax algo to return the best move
 
 	def getAllNextMoves(self, b):
 		pcs = b.getPieces(b.turn)
@@ -116,3 +96,86 @@ class gSmart:
 		print("total weight: " + totalWeight)
 		
 		return totalWeight
+
+	class gameTree():
+
+		def __init__(self, b, n):				#builds a game tree of "n" ply from board "b"
+			self.t = gSmart.gameTree.tree(b)	#create a tree
+			cur = self.t.getRoot()				#grab the root
+			self.addPly(cur, b, 3)				#build out "h" ply
+
+		def addPly(self, curNode, b, ply):
+			if ply == 0:			#basecase
+				return
+			else:
+				moves = getAllNextMoves(curNode.board)	#get moves for board in current node
+				for move in moves:						
+					temp = gameTree.tree.node(b,move,mm)		#make a new node for each move
+					curNode.addChild(temp)						#add the new node as a child to curNode
+					self.addPly(temp, b, ply-1)					#recursively call addPly on the child, with one less ply
+		
+		def getMinOrMax(self, b):
+			if b.getTurn == "w":
+				return "max"
+			else:
+				return "min"
+				
+		def minimax(self):
+			return None
+
+		class tree:
+		
+			def __init__(self, b = None, m= None):
+				self.root = gSmart.gameTree.tree.node(b, m)
+		
+			def getRoot(self):
+				return self.root
+				
+			def addNode(self, parent, child):
+				parent.addChild(child)
+		
+			def DFS(self, start):
+				print(str(start))
+				children = start.getChildren()
+				if(len(children) == 0):
+					return
+				else:
+					for child in children:
+						self.DFS(child)
+				
+			class node:
+			
+				def __init__(self, b = None, m = None):
+					self.children = []
+					self.board = b
+					self.move = m
+					self.value = None
+				
+				def addChild(self, newChild):
+					self.children.append(newChild)
+				
+				def getChildren(self):
+					return self.children
+					
+				def getData(self):
+					return self.data
+					
+				def setValue(self, v):
+					if v == None:
+						self.value = self.getBoardValue()
+					else:
+						self.value = v
+					
+				def getValue(self):
+						return self.value
+						
+				def getBoardValue(self):
+					return self.gSmart.evaluatePosition()
+				
+				def isMaxNode(self):
+					return self.board.isTurn() == "w"
+
+bd = board.Board()
+bd.setupDefault()
+gt = gSmart.gameTree(bd, 3)
+t.DFS(gt.getRoot())
